e2a220f5af58c7c43d8cf4edf89bcfa8bf55bf36
Lim Joe Quan <joequanlim@gmail.com>
2026-02-18 19:23:16 +0800

    Prepare psuedocode

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './styles.css' 
+
+const form = document.querySelector('form')
+const submit = document.querySelector('button')
+let isValid = false
+form.addEventListener('input', (target) => {
+  //js validation code here
+})
+submit.addEventListener('click', () => {
+  if (isValid === true) {
+    //submit form code here
+  } else {
+    //invalid form code here
+  }
+})
